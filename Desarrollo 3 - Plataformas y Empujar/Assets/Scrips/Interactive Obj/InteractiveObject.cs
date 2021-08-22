40a4be8bb505b2c3c83865488df1f7cd89a89803
Tomas <Aquista35@gmail.com>
2021-08-21 21:07:24 -0300

    Added Rooms and Wooden Chest

---
+++ Desarrollo 3 - Plataformas y Empujar/Assets/Scrips/Interactive Obj/InteractiveObject.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractiveObject : MonoBehaviour
+{
+    public enum StateObject
+    {
+        Closed,
+        Available,
+        Open
+    };
+
+    public GameObject posterUI;
+
+    public StateObject stateObject;
+
+    Animator animator;
+    bool inRange = false;
+
+    //========================================
+
+    private void Start()
+    {
+        posterUI.SetActive(false);
+
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (stateObject == StateObject.Available)
+        {
+            posterUI.SetActive(true);
+
+            inRange = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (stateObject == StateObject.Available && inRange == true)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                stateObject = StateObject.Open;
+                posterUI.SetActive(false);
+                animator.SetTrigger("Open");
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (stateObject == StateObject.Available)
+        {
+            posterUI.SetActive(false);
+
+            inRange = false;
+        }
+    }
+}
---
+++ Desarrollo 3 - Plataformas y Empujar/Assets/Scrips/Pillars/SetRoom.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetRoom : MonoBehaviour
+{
+    public Transform parentRoom;
+
+    public List<GameObject> rooms;
+
+    //===============================
+
+    private void OnEnable()
+    {
+        int random = Random.Range(0, rooms.Count);
+
+        var go = Instantiate(rooms[random], parentRoom);
+        go.transform.name = rooms[random].name;
+    }
+
+}
