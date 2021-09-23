03658b6737c108edb04c959a5c2ad57a8dcfa2ce
Tomas <Aquista35@gmail.com>
2021-09-23 19:47:59 -0300

    Added Pause layer and Jack damage

---
+++ Desarrollo 3 - Plataformas y Empujar/Assets/Scripts/Scenes Controller/MenuCanvas.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuCanvas : MonoBehaviour
+{
+    public void CloseLayer(GameObject layer)
+    {
+        layer.SetActive(false);
+    }
+
+    public void GoToScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void ChangeLayer(string name)
+    {
+
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
