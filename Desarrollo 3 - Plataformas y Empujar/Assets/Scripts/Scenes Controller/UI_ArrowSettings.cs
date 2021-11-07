d72b10063e80786493c80d9b1e83724332c4b3b5
Tomas <Aquista35@gmail.com>
2021-11-07 20:05:25 -0300

    Added Controlls in the Settings

---
+++ Desarrollo 3 - Plataformas y Empujar/Assets/Scripts/Scenes Controller/UI_ArrowSettings.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class UI_ArrowSettings : MonoBehaviour
+{
+    public Transform arrow;
+
+    public void SetArrowPosition(int posY)
+    {
+        arrow.transform.localPosition = new Vector2(arrow.localPosition.x, posY);
+    }
+}
