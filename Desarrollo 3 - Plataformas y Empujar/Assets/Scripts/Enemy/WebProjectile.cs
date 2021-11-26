996b966bc35783bf0edb503f845b9015105ef4ea
Tomas <Aquista35@gmail.com>
2021-11-26 18:50:33 -0300

    Fixed spider web

--- Desarrollo 3 - Plataformas y Empujar/Assets/Scripts/Enemy/WebProjectile.cs
+++ Desarrollo 3 - Plataformas y Empujar/Assets/Scripts/Enemy/WebProjectile.cs
@@ -25,16 +25,21 @@ public class WebProjectile : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        PlayerMovement target = collision.collider.gameObject.GetComponentInParent<PlayerMovement>();
+        //PlayerMovement target = collision.collider.gameObject.GetComponentInParent<PlayerMovement>();
         
-        if(target != null)
-        {
-            StartCoroutine(target.Slow(slowStrength, slowDuration));
-            Destroy(gameObject);
-        }
-        else if(collision.collider.gameObject.tag == "Terrain")
+        //if(target != null)
+        //{
+        //    StartCoroutine(target.Slow(slowStrength, slowDuration));
+        //    Destroy(gameObject);
+        //}
+        //else
+        if(collision.collider.gameObject.tag == "Terrain")
         {
             GameObject go = Instantiate(trap,transform.position, Quaternion.identity, collision.collider.transform);
+
+            go.transform.localScale = new Vector3(go.transform.localScale.x / collision.transform.localScale.x,
+                                                  go.transform.localScale.y / collision.transform.localScale.y,
+                                                  go.transform.localScale.z / collision.transform.localScale.z);
 
             AkSoundEngine.PostEvent("telarana_abre", gameObject);
 
